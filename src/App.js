201cc5c2badcc394046c4952f52c90e6147f1c0f
sreshma-006 <60685795+sreshma-006@users.noreply.github.com>
2022-03-21 18:48:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import EDITOR from './components/Editor'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <EDITOR/>
+    </div>
+  );
+}
+
+export default App;
